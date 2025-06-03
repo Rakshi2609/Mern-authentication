be69925434a36c44dd3cb74041cc379a81dec313
Rakshi2609 <rakshithganjimut@gmail.com>
2025-06-03 08:56:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,38 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import path from "path";
-
-import { connectDB } from "./db/connectDB.js";
-
-import authRoutes from "./routes/auth.route.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connectDB } from './db/connectDB.js';
+import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(express.json()); // allows us to parse incoming requests:req.body
-app.use(cookieParser()); // allows us to parse incoming cookies
+app.get('/', (req, res) => {
+    res.send('Hello, Wor1ld!');
+});
+
+app.get("/test-cookie", (req, res) => {
+  console.log("Cookies:", req.cookies);
+  res.send("Check console");
+});
 
 app.use("/api/auth", authRoutes);
-
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    });
 }
 
 app.listen(PORT, () => {
-	connectDB();
-	console.log("Server is running on port: ", PORT);
+    console.log(`Server is running on ${PORT}`);
+    connectDB()
 });
