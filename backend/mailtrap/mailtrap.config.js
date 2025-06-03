4b0e47f754904367f4632c269d7ba8e3dab3af8e
Rakshi2609 <rakshithganjimut@gmail.com>
2025-06-03 20:49:48 +0530

    Update mailtrap.config.js

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -9,6 +9,6 @@ export const mailtrapClient = new MailtrapClient({
 });
 
 export const sender = {
-	email: "mailtrap@demomailtrap.com",
-	name: "Burak",
+	email: "btchcodes@gmail.com",
+	name: "Sonira",
 };
